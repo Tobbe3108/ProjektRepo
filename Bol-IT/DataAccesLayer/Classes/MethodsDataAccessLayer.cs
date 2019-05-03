8fb71e96d9bde358416e4a3f7be2c4bcc1f8089d
Christoffer Pedersen <chriskpedersen@hotmail.com>
2019-05-03 09:33:55 +0200

    Den har ikke fejl mere

--- Bol-IT/DataAccesLayer/Classes/MethodsDataAccessLayer.cs
+++ Bol-IT/DataAccesLayer/Classes/MethodsDataAccessLayer.cs
@@ -23,11 +23,10 @@ namespace DataAccesLayer.Classes
         public static mydatabasetobbeDataSetTableAdapters.worksWithTableAdapter worksWithTableAdapter = new mydatabasetobbeDataSetTableAdapters.worksWithTableAdapter();
         #endregion TableAdapters
 
-        public static void CreateAgent(string fName, string mName, string lName)
+        public static void CreateAgent(string fName, string mName, string lName, int phoneNr, string address, int zipcode, string mail, int nrOfSales)
         {
-            int id = personalDataTableAdapter.GetDataByName(fName, mName, lName);
-            personalDataTableAdapter.GetDataByNames();
-            agentTableAdapter.Insert(id, 87);
+            int id = personalDataTableAdapter.InsertData(fName, mName, lName, phoneNr, address, zipcode, mail);
+            agentTableAdapter.InsertData(id, nrOfSales);
 
         }
     }
