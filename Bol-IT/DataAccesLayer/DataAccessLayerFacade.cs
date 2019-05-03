a509bdf16d4adaeb0df8a9a9559dddda3c4beeee
Tobias <Tobbe3108@gmail.com>
2019-05-03 09:55:58 +0200

    DataAccessLayerFacade tableadapters

--- Bol-IT/DataAccesLayer/DataAccessLayerFacade.cs
+++ Bol-IT/DataAccesLayer/DataAccessLayerFacade.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Data.SqlClient;
 using BusinessLayer.Classes;
 using System.Configuration;
+using DataAccessLayer.mydatabasetobbeDataSetTableAdapters;
 
 namespace DataAccessLayer
 {
@@ -13,5 +14,69 @@ namespace DataAccessLayer
         {
             return new SqlConnection(connectionString: ConfigurationManager.AppSettings["AzureDB"]);
         }
+
+        public static saleTableAdapter saleTableAdapter()
+        {
+            return MethodsDataAccessLayer.saleTableAdapter;
+        }
+
+        public static agentTableAdapter agentTableAdapter()
+        {
+            return MethodsDataAccessLayer.agentTableAdapter;
+        }
+
+        public static buyerTableAdapter buyerTableAdapter()
+        {
+            return MethodsDataAccessLayer.buyerTableAdapter;
+        }
+
+        public static filesTableAdapter filesTableAdapter()
+        {
+            return MethodsDataAccessLayer.filesTableAdapter;
+        }
+
+        public static sellerTableAdapter sellerTableAdapter()
+        {
+            return MethodsDataAccessLayer.sellerTableAdapter;
+        }
+
+        public static TableAdapterManager TableAdapterManager()
+        {
+            return MethodsDataAccessLayer.TableAdapterManager;
+        }
+
+        public static propertyTableAdapter propertyTableAdapter()
+        {
+            return MethodsDataAccessLayer.propertyTableAdapter;
+        }
+
+        public static worksWithTableAdapter worksWithTableAdapter()
+        {
+            return MethodsDataAccessLayer.worksWithTableAdapter;
+        }
+
+        public static assesmentTableAdapter assesmentTableAdapter()
+        {
+            return MethodsDataAccessLayer.assesmentTableAdapter;
+        }
+
+        public static wantsToSellTableAdapter wantsToSellTableAdapter()
+        {
+            return MethodsDataAccessLayer.wantsToSellTableAdapter;
+        }
+
+        public static personalDataTableAdapter personalDataTableAdapter()
+        {
+            return MethodsDataAccessLayer.personalDataTableAdapter;
+        }
+
+        public static externalContactsTableAdapter externalContactsTableAdapter()
+        {
+            return MethodsDataAccessLayer.externalContactsTableAdapter;
+        }
+
+
+
+
     }
 }
