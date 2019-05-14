ee1b3f551c39ae82298b48ae0f17f1ceb18f7585
Nanami <Skitsine@gmail.com>
2019-05-14 10:38:03 +0200

    Added comments to part of the code

--- Bol-IT/BusinessLayer/BusinessLayerFacade.cs
+++ Bol-IT/BusinessLayer/BusinessLayerFacade.cs
@@ -38,26 +38,58 @@ namespace BusinessLayer
         #endregion
 
         //Simone
+        /// <summary>
+        /// Beregner brutto prisen ud fra total prisen
+        /// </summary>
+        /// <param name="cashPrice"></param>
+        /// <returns></returns>
         public static int CalculateGrossPrice(int cashPrice)
         {
             return PriceCalculator.CalculateGrossPrice(cashPrice);
         }
 
+        /// <summary>
+        /// Beregner netto prisen ud fra total prisen  
+        /// </summary>
+        /// <param name="cashPrice"></param>
+        /// <returns></returns>
         public static int CalculateNetPrice(int cashPrice)
         {
             return PriceCalculator.CalculateNetPrice(cashPrice);
         }
 
+        /// <summary>
+        /// Beregner ejerudgiften ud fra total prisen
+        /// </summary>
+        /// <param name="cashPrice"></param>
+        /// <returns></returns>
         public static int CalculateOwnerExpences(int cashPrice)
         {
             return PriceCalculator.CalculateOwnerExpences(cashPrice);
         }
 
+        /// <summary>
+        /// Beregner udbetalingen ud fra total prisen
+        /// </summary>
+        /// <param name="cashPrice"></param>
+        /// <returns></returns>
         public static int CalculateDepositPrice(int cashPrice)
         {
             return PriceCalculator.CalculateDepositPrice(cashPrice);
         }
 
+        /// <summary>
+        /// Beregner total prisen ud fra grund areal, postnummer, stand, indretning, stil, køkken, badeværelse og have
+        /// </summary>
+        /// <param name="propSquareMeter"></param>
+        /// <param name="zipcode"></param>
+        /// <param name="condition"></param>
+        /// <param name="interiorDesign"></param>
+        /// <param name="style"></param>
+        /// <param name="kitchen"></param>
+        /// <param name="bathroom"></param>
+        /// <param name="gardenFlag"></param>
+        /// <returns></returns>
         public static int CalculateCashPrice(int propSquareMeter, int zipcode, string condition, string interiorDesign, string style, string kitchen, string bathroom, bool gardenFlag)
         {
             return PriceCalculator.CalculateCashPrice(propSquareMeter, zipcode, condition, interiorDesign, style, kitchen, bathroom, gardenFlag);
