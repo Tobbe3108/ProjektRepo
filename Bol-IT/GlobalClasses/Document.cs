15b34bc7fc7eb0cc9fe337930a2a785fc2abb718
Christoffer Pedersen <chri077r@edu.eal.dk>
2019-05-20 02:56:44 +0200

    Added a document.

---
+++ Bol-IT/GlobalClasses/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalClasses
+{
+    public class Document
+    {
+        public int CaseNr { get; set; }
+        public string Name { get; set; }
+        public string Extention { get; set; }
+        public Byte[] Data { get; set; }
+    }
+}
